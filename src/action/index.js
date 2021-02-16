2a0901f4bb754fa2a7384c449f1a1cff8d3360ec
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2021-02-17 00:30:49 +0200

    dimaarion/react-game.git

---
+++ src/action/index.js
@@ -0,0 +1,7 @@
+export const arrayCount = (count) => {
+  let arr = [];
+  for (let i = 0; i < count; i++) {
+    arr[i] = i;
+  }
+  return arr;
+};
--- src/road/Earth.js
+++ src/road/Earth.js
@@ -1,8 +1,13 @@
+import road from "../db/road.json";
+import { arrayCount } from "../action";
 export default function Earth(p5) {
-  return p5.loadImage(
-    "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/YKz9-earth.png",
-    (img) => {
-      p5.image(img, 0, 0);
-    }
-  );
+  return p5.loadImage(road.earth.img, (img) => {
+    arrayCount(road.earth.parametr.count).map((x) =>
+      p5.image(
+        img,
+        x * road.earth.parametr.width,
+        window.innerHeight - road.earth.parametr.height
+      )
+    );
+  });
 }
