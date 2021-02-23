3a2a0e85e5ec692d22e3bf4ccdfa9a1a54541ecd
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2021-02-24 01:24:21 +0200

    dimaarion/react-game.git

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import scena from "./db/scena.json";
 import ErathMap from "./road/ErathMap";
 import { kolobokGoRight } from "./action";
 export default function App() {
-  let imgErath, imgKolobokFas, imgKolobokLeft;
+  let imgErath,
+    imgKolobokFas,
+    imgKolobokLeft,
+    imgKolobokRight,
+    imgKolobokFasInvert,
+    imgKolobokLeftInvert,
+    imgKolobokJamp,
+    imgKolobokRightInvert;
 
   const preload = (p5) => {
     imgErath = p5.loadImage(
@@ -16,6 +23,21 @@ export default function App() {
     );
     imgKolobokLeft = p5.loadImage(
       "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/Bjz--kolobokLeft.png"
+    );
+    imgKolobokRight = p5.loadImage(
+      "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/btaw-kolobokRight.png"
+    );
+    imgKolobokFasInvert = p5.loadImage(
+      "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/j5cI-kolobokFasInvert.png"
+    );
+    imgKolobokLeftInvert = p5.loadImage(
+      "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/ElC3-kolobokLeftInvert.png"
+    );
+    imgKolobokRightInvert = p5.loadImage(
+      "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/uxeZ-kolobokRightInvert.png"
+    );
+    imgKolobokJamp = p5.loadImage(
+      "https://uploads.codesandbox.io/uploads/user/f0ec9a1a-dbb6-4f1c-875a-49dd16e23056/ZAhk-koloborJamp.png"
     );
   };
   const props = {
@@ -37,16 +59,24 @@ export default function App() {
     );
 
     ErathMap(p5, propsStart);
-    kolobok(p5, propsStart);
+    // kolobok(p5, propsStart);
   };
   let speed = 0;
   let presed = 0;
+  let presedTop = 0;
   let spedKadr = 0;
   const draw = (p5) => {
     ErathMap(p5, { imgErath: imgErath });
     kolobok(p5, {
       imgKolobokFas: imgKolobokFas,
+      imgKolobokLeft: imgKolobokLeft,
+      imgKolobokRight: imgKolobokRight,
+      imgKolobokFasInvert: imgKolobokFasInvert,
+      imgKolobokLeftInvert: imgKolobokLeftInvert,
+      imgKolobokRightInvert: imgKolobokRightInvert,
+      imgKolobokJamp: imgKolobokJamp,
       presed: presed,
+      presedTop: presedTop,
       spedKadr: spedKadr
     });
   };
@@ -56,12 +86,18 @@ export default function App() {
     } else if (p5.keyCode === p5.RIGHT_ARROW) {
       presed = 2;
     }
+    if (p5.keyCode === 38) {
+      presedTop = 3;
+    }
   };
   const keyReleased = (p5) => {
     if (p5.keyCode === p5.LEFT_ARROW) {
       presed = 0;
     } else if (p5.keyCode === p5.RIGHT_ARROW) {
       presed = 0;
+    }
+    if (p5.keyCode === 38) {
+      presedTop = 0;
     }
   };
   return (
--- src/kolobok/Kolobok.js
+++ src/kolobok/Kolobok.js
@@ -1,15 +1,73 @@
-export default function Kolobok(props) {
-  props.scena.layers
+import scena from "../db/scena.json";
+import image from "../db/image.json";
+export default function Kolobok(p5, props) {
+  let imgArr = [
+    props.imgKolobokFas,
+    props.imgKolobokRight,
+    props.imgKolobokFas,
+    props.imgKolobokLeft
+  ];
+  let imgArrInvert = [
+    props.imgKolobokFasInvert,
+    props.imgKolobokRightInvert,
+    props.imgKolobokFasInvert,
+    props.imgKolobokLeftInvert
+  ];
+  let speed = 40;
+
+  scena.layers
     .filter((x) => x.type === "objectgroup")
     .map((x2, i) => {
       if (x2.objects[i].type === "users") {
-        props.p5.image(
-          props.imgKolobok,
-          x2.objects[i].x,
-          x2.objects[i].y,
-          x2.objects[i].width,
-          x2.objects[i].height
-        );
+        if (props.presedTop === 3) {
+          p5.frameRate(5);
+          p5.image(
+            props.imgKolobokJamp,
+            (x2.objects[i].x += speed),
+            (x2.objects[i].y -= speed),
+            x2.objects[i].width,
+            x2.objects[i].height
+          );
+        } else {
+          if (props.presed === 2) {
+            image.imgAnimation.start += 1;
+            if (image.imgAnimation.start === imgArr.length) {
+              image.imgAnimation.start = 0;
+            }
+            p5.frameRate(16);
+            p5.image(
+              imgArr[image.imgAnimation.start],
+              (x2.objects[i].x += speed),
+              x2.objects[i].y,
+              x2.objects[i].width,
+              x2.objects[i].height
+            );
+          }
+          if (props.presed === 1) {
+            image.imgAnimation.start += 1;
+            if (image.imgAnimation.start === imgArrInvert.length) {
+              image.imgAnimation.start = 0;
+            }
+            p5.frameRate(16);
+            p5.image(
+              imgArrInvert[image.imgAnimation.start],
+              (x2.objects[i].x -= speed),
+              x2.objects[i].y,
+              x2.objects[i].width,
+              x2.objects[i].height
+            );
+          }
+
+          if (props.presed === 0) {
+            p5.image(
+              props.imgKolobokFas,
+              x2.objects[i].x,
+              x2.objects[i].y,
+              x2.objects[i].width,
+              x2.objects[i].height
+            );
+          }
+        }
       }
     });
 }
